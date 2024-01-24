1572edcb9733b5ba7604862f7381fa2048c3ab9d
Nagy Zétény <nagyzeteny6@gmail.com>
2024-01-24 18:04:48 +0100

    prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,18 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
+  tailwindFunctions: ['cn'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 
   tabWidth: 2,
   useTabs: false,
